75d341770219ba38fa52c8daca50af26258d71c8
NToepke <nathantoepke@gmail.com>
2022-12-12 11:59:51 -0600

    Update runparser.bat
    Added handling for better ANTLR compiler handling.

--- runparser.bat
+++ runparser.bat
@@ -2,7 +2,7 @@
 java -cp ".\antlr-4.9.2-complete.jar;%CLASSPATH%" org.antlr.v4.Tool PyGrammar.g4
 
 :: Compile
-javac PyGrammar*.java
+javac -cp ".\antlr-4.9.2-complete.jar;%CLASSPATH%" PyGrammar*.java
 
 :: Execute the code on the given python script
 type %* | java -cp ".\antlr-4.9.2-complete.jar;%CLASSPATH%" org.antlr.v4.gui.TestRig PyGrammar program -gui

ac19952848b4af0a3f9a8bf4e87e6a4a1950fb0e
Greyson Spencer <greyspencer1@gmail.com>
2022-12-12 11:36:32 -0600

    Update java commands to run as expected

--- runparser.bat
+++ runparser.bat
@@ -1,8 +1,8 @@
 :: Generate
-java org.antlr.v4.Tool PyGrammar.g4
+java -cp ".\antlr-4.9.2-complete.jar;%CLASSPATH%" org.antlr.v4.Tool PyGrammar.g4
 
 :: Compile
 javac PyGrammar*.java
 
 :: Execute the code on the given python script
-type %* | java org.antlr.v4.gui.TestRig PyGrammar program -gui
+type %* | java -cp ".\antlr-4.9.2-complete.jar;%CLASSPATH%" org.antlr.v4.gui.TestRig PyGrammar program -gui
